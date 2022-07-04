19f68e50538a9cff8d5058624011d5bce9871028
21cloudocean <102290668+21cloudocean@users.noreply.github.com>
2022-07-04 11:18:41 +0800

    completed JS: prototype & class refactor

---
+++ app-prototype.js
@@ -0,0 +1,122 @@
+function getElement(selection) {
+  const element = document.querySelector(selection);
+  if (element) {
+    return element;
+  }
+  throw new Error(
+    `Please check "${selection}" selector, no such element exists`
+  );
+}
+
+// 获取每个section的图片、modal、按钮
+function Gallery(element) {
+  this.container = element;
+  // this.list = element.querySelectorAll(".img");
+  //把上面的nodelist转换为array
+  this.list = [...element.querySelectorAll(".img")];
+  // target
+  this.modal = getElement(".modal");
+  this.modalImg = getElement(".main-img");
+  this.imageName = getElement(".image-name");
+  this.modalImages = getElement(".modal-images");
+  this.closeBtn = getElement(".close-btn");
+  this.nextBtn = getElement(".next-btn");
+  this.prevBtn = getElement(".prev-btn");
+  //self reference
+  // let self = this;
+  // bind functions
+  //使用openModal时我们想要的效果是this指向gallery，而不是section。
+  // this.openModal = this.openModal.bind(this);
+  this.closeModal = this.closeModal.bind(this);
+  this.nextImage = this.nextImage.bind(this);
+  this.prevImage = this.prevImage.bind(this);
+  this.chooseImage = this.chooseImage.bind(this);
+  // container event
+  this.container.addEventListener(
+    "click",
+    function (e) {
+      // self.openModal();
+      if (e.target.classList.contains("img")) {
+        this.openModal(e.target, this.list);
+      }
+    }.bind(this)
+  );
+}
+
+Gallery.prototype.openModal = function (selectedImage, list) {
+  this.setMainImage(selectedImage);
+  this.modalImages.innerHTML = list
+    .map(function (image) {
+      // console.log(image.src, image.title, image.dataset.id);
+      return `          <img
+            src="${image.src}"
+            title="${image.title}"
+            class="${
+              selectedImage.dataset.id === image.dataset.id
+                ? "modal-img selected"
+                : "modal-img"
+            }"
+            data-id="${image.dataset.id}"
+            alt="city"
+          />`;
+    })
+    .join("");
+  this.modal.classList.add("open");
+  this.closeBtn.addEventListener("click", this.closeModal);
+  this.nextBtn.addEventListener("click", this.nextImage);
+  this.prevBtn.addEventListener("click", this.prevImage);
+  this.modalImages.addEventListener("click", this.chooseImage);
+};
+
+Gallery.prototype.setMainImage = function (selectedImage) {
+  this.modalImg.src = selectedImage.src;
+  this.imageName.textContent = selectedImage.title;
+};
+
+// 三个按钮对应的method
+Gallery.prototype.closeModal = function () {
+  this.modal.classList.remove("open");
+  this.closeBtn.removeEventListener("click", this.closeModal);
+  this.nextBtn.removeEventListener("click", this.nextImage);
+  this.prevBtn.removeEventListener("click", this.prevImage);
+  this.modalImages.removeEventListener("click", this.chooseImage);
+};
+
+Gallery.prototype.nextImage = function () {
+  // console.log(this);
+  const selected = this.modalImages.querySelector(".selected");
+  // 选择下一张图片：如果没有下一张图片，则选择第一张图片
+  const next =
+    selected.nextElementSibling || this.modalImages.firstElementChild;
+  selected.classList.remove("selected");
+  next.classList.add("selected");
+  this.setMainImage(next);
+  // console.log("nextImage");
+};
+
+Gallery.prototype.prevImage = function () {
+  const selected = this.modalImages.querySelector(".selected");
+  // 选择上一张图片：如果没有上一张图片，则选择最后一张图片
+  const prev =
+    selected.previousElementSibling || this.modalImages.lastElementChild;
+  selected.classList.remove("selected");
+  prev.classList.add("selected");
+  this.setMainImage(prev);
+  // console.log("prevImage");
+};
+
+// 点击选择图片
+Gallery.prototype.chooseImage = function (e) {
+  if (e.target.classList.contains("modal-img")) {
+    // 列表中显示选中了该图片
+    const selected = this.modalImages.querySelector(".selected");
+    selected.classList.remove("selected");
+    e.target.classList.add("selected");
+    // 将该图片设为main-image
+    this.setMainImage(e.target);
+  }
+};
+
+// select element 选择section
+const nature = new Gallery(getElement(".nature"));
+const city = new Gallery(getElement(".city"));
